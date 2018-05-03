70fad587a718059718c4664c026ae7bcee1faecd
igrad <iangradert@gmail.com>
2018-05-03 07:11:52 -0500

    - Move loading funcs from index.js back to load.js - Finish loading page data to screen - Finish saving edited page content to file

--- js/index.js
+++ js/index.js
@@ -67,88 +67,23 @@ function TreeViewSwitch(caller) {
 
 
 
-function AddTreeNodeToScreen(id, depth) {
-   var node = notebookData.GetNode(id)
-   var treeView = $("#tree_view")[0]
-
-   // Add the node itself to the screen
-   var indent = (parseInt($("body").css('--treeview-depth-indent')) * (depth - 1)) + "px"
-
-   var insertHTML = "<div class=\"tree_view_item\" id=\"" + id + "\" "
-   insertHTML += "onmouseover=\"TreeViewHover(this)\" "
-   insertHTML += "onmouseout=\"TreeViewLeaveHover(this)\" "
-   insertHTML += "onclick=\"TreeViewSwitch(this)\" "
-   insertHTML += "style=\"left: " + indent + "\">" + node.name + "</div>"
-   treeView.innerHTML += insertHTML
-
-   // Add the nodes children to the screen by recursively calling this function
-   for (var i = 0; i < node.children.length; i++) {
-      AddTreeNodeToScreen(node.children[i], depth + 1)
-   }
+// Helper function for ContentEdited function
+function sleep (ms) {
+   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 
 
-function LoadNotebookToScreen(id) {
+async function ContentEdited() {
+   var tEdit = Date.now()
+   lastEditTime  = tEdit
+   await sleep(1000)
 
-   // TODO: Wrap all of this in a try-catch
-
-   // Order of events
-   // 1. Load the theme of the notebook onto screen
-   // 2. Load the nodes into tree view
-
-   // Load the theme of the notebook onto screen
-   // Get the currently-loaded theme's outer HTML string
-   var linkTheme = $("#link_theme")[0]
-   var linkThemeText = linkTheme.outerHTML
-
-   // Isolate just the file name of the current theme
-   var currentTheme = linkThemeText.substring((linkThemeText.indexOf("/") + 1), (linkThemeText.indexOf(".css")))
-
-   // Replace the current theme with the new theme of notebook being loaded
-   linkTheme.outerHTML = linkThemeText.replace(currentTheme, notebookData.theme)
-
-
-   // Load the nodes into the tree view
-   // Get the tree view object
-   var treeView = $("#tree_view")[0]
-
-
-   // Clear out the existing contents
-   treeView.innerHTML = ""
-
-   // Traverse the linked list/binary tree in order and build each node in memory
-   // While doing this, also add each label into the content view
-   var key = "0-0"
-   var depth = 0
-   var rootnode = notebookData.GetNode("0-0")
-   var children = rootnode.children
-
-   if (children.length == 0) {
-      // Add a new node to the tree, then push it to the screen
-   } else {
-      for (var i = 0; i < children.length; i++) {
-         AddTreeNodeToScreen(children[i], 1)
-      }
+   // If the last edit time is still the same time as when this function was called,
+   // then this edit was the last edit made before the user paused. Save data to file.
+   if (lastEditTime == tEdit) {
+      SavePageToFile(lastOpenPage)
    }
-
-   console.log("Load notebook to screen")
-   SetLastOpenNoteBook(id)
-}
-
-
-
-function LoadPageToScreen(id) {
-   console.log("Loading page " + id)
-
-   var node = notebookData.GetNode(id)
-   var pathtopage = "./pages/" + node.data + ".html"
-
-   SetLastOpenPage(id)
-
-   var pageData = LoadFileData(pathtopage)
-
-   $("#content_view")[0].innerHTML = pageData
 }
 
 
--- js/load.js
+++ js/load.js
@@ -4,6 +4,8 @@ const appVersion = '0.0.1'
 let sessionData
 let notebookData
 
+let lastEditTime = new Date()
+
 let lastRunTime = null
 let lastRunVersion = null
 let lastOpenNotebook = null
@@ -36,14 +38,20 @@ function LoadSession() {
    console.log("======================")
 }
 
+
+
 function SetLastOpenNoteBook (notebookID) {
    sessionData.set("LASTOPENNOTEBOOK", notebookID)
 }
+
+
 
 function SetLastOpenPage (pageID) {
    lastOpenPage = pageID
    sessionData.set("LASTOPENPAGE", pageID)
 }
+
+
 
 function LoadFileData (filepath) {
    const fs = require('fs')
@@ -51,7 +59,116 @@ function LoadFileData (filepath) {
    try {
       var dat = fs.readFileSync(filepath)
       return dat.toString()
-   } catch(error) {
+   } catch (error) {
       console.log("Could not load file " + filepath)
    }
+}
+
+
+
+function SaveFileData (filepath, data) {
+   const fs = require('fs')
+
+   try {
+      fs.writeFileSync(filepath, data)
+   } catch (error) {
+      console.log("Could not save data to file " + filepath)
+   }
+}
+
+
+
+function AddTreeNodeToScreen(id, depth) {
+   var node = notebookData.GetNode(id)
+   var treeView = $("#tree_view")[0]
+
+   // Add the node itself to the screen
+   var indent = (parseInt($("body").css('--treeview-depth-indent')) * (depth - 1)) + "px"
+
+   var insertHTML = "<div class=\"tree_view_item\" id=\"" + id + "\" "
+   insertHTML += "onmouseover=\"TreeViewHover(this)\" "
+   insertHTML += "onmouseout=\"TreeViewLeaveHover(this)\" "
+   insertHTML += "onclick=\"TreeViewSwitch(this)\" "
+   insertHTML += "style=\"left: " + indent + "\">" + node.name + "</div>"
+   treeView.innerHTML += insertHTML
+
+   // Add the nodes children to the screen by recursively calling this function
+   for (var i = 0; i < node.children.length; i++) {
+      AddTreeNodeToScreen(node.children[i], depth + 1)
+   }
+}
+
+
+
+function LoadNotebookToScreen(id) {
+
+   // TODO: Wrap all of this in a try-catch
+
+   // Order of events
+   // 1. Load the theme of the notebook onto screen
+   // 2. Load the nodes into tree view
+
+   // Load the theme of the notebook onto screen
+   // Get the currently-loaded theme's outer HTML string
+   var linkTheme = $("#link_theme")[0]
+   var linkThemeText = linkTheme.outerHTML
+
+   // Isolate just the file name of the current theme
+   var currentTheme = linkThemeText.substring((linkThemeText.indexOf("/") + 1), (linkThemeText.indexOf(".css")))
+
+   // Replace the current theme with the new theme of notebook being loaded
+   linkTheme.outerHTML = linkThemeText.replace(currentTheme, notebookData.theme)
+
+
+   // Load the nodes into the tree view
+   // Get the tree view object
+   var treeView = $("#tree_view")[0]
+
+
+   // Clear out the existing contents
+   treeView.innerHTML = ""
+
+   // Traverse the linked list/binary tree in order and build each node in memory
+   // While doing this, also add each label into the content view
+   var key = "0-0"
+   var depth = 0
+   var rootnode = notebookData.GetNode("0-0")
+   var children = rootnode.children
+
+   if (children.length == 0) {
+      // Add a new node to the tree, then push it to the screen
+   } else {
+      for (var i = 0; i < children.length; i++) {
+         AddTreeNodeToScreen(children[i], 1)
+      }
+   }
+
+   console.log("Load notebook to screen")
+   SetLastOpenNoteBook(id)
+}
+
+
+
+function LoadPageToScreen(id) {
+   console.log("Loading page " + id)
+   
+   var pathtopage = "./pages/" + id + ".html"
+
+   SetLastOpenPage(id)
+
+   var pageData = LoadFileData(pathtopage)
+
+   console.log("Loading page from " + pathtopage)
+
+   $("#content_view")[0].innerHTML = pageData
+}
+
+
+
+function SavePageToFile(id) {
+   console.log("Saving page " + id + " to file")
+   var pathtopage = "./pages/" + id + ".html"
+   var pageData = $("#content_view")[0].innerHTML
+
+   SaveFileData(pathtopage, pageData)
 }
