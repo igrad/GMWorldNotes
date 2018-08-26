5cfd939c32ffa63343ae8d83f9f068bac8943e59
Ian Gradert <iangradert@gmail.com>
2018-08-25 20:39:45 -0500

    - Add generated-fonts.css file to host dynamically-loaded fonts - Set up system font loading

--- js/lower-shelf.js
+++ js/lower-shelf.js
@@ -1,8 +1,32 @@
-function CreateFontFamilyOptions() {
-   var fontList = sysFonts.getFonts().then(
-      function(res) { return res },
-      function(err) { throw "Failed to load fonts" }
+async function CreateFontFamilyOptions() {
+   var fontList = await sysFonts.getFonts().then(
+      function(res) {
+         console.log("Loaded " + res.length + " fonts.")
+         return res
+      },
+      function(err) {
+         console.log("Failed to load fonts")
+         return []
+      }
    )
+
+   // Use @font-face in addition with local() function for each named font family
+   // List each of the created fonts in the generated-fonts sheet
+   var sheet = document.styleSheets[3]
+
+   for (var i = 0; i < fontList.length; i++) {
+      var fontName = fontList[i]
+
+      try {
+         sheet.insertRule("@font-face {font-family: '" + fontName + "'; src: local('" + fontName + "');}", 0)
+      } catch (err) {
+         console.log("Failed to load font: " + fontName)
+
+         // Remove the failed fonts
+         var index = fontList.indexOf(fontName)
+         fontList.splice(index, 1)
+      }
+   }
 }
 
 
